0e106753feb3ebba2d8cccc22afe93d6a0c1bf69
tomedwrds <tomedwards1056@outlook.com>
2023-04-07 20:08:42 +1200

    still working on moving project to vite, quite busy with uni atm

---
+++ waaiers-vite/src/private/APIKey.js
@@ -0,0 +1,8 @@
+//The API Key needs to be kept private so it can be stolen and used
+
+function getMetServiceApiKey()
+{
+    return 'DqZtGZuStNe89kAp1sgqSy';
+}
+
+export default getMetServiceApiKey;
