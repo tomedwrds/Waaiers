aecf4ef7ab51ea4dee9f2b3ac501556943b06fde
tomedwrds <tomedwards1056@outlook.com>
2023-05-02 14:42:32 +1200

    finished admin pannel, weather for routes can now be updated by admin

--- waaiers-vite/src/AdminRaceTab.jsx
+++ waaiers-vite/src/AdminRaceTab.jsx
@@ -1,7 +1,6 @@
 import RefreshWeatherData from "./RefreshWeatherData"
 const AdminRaceTab = (props) =>{
     
-    console.log(props.data)
     return(
         <div style = {{display:"flex"}}>
             <p>{props.data.route_name}</p>
--- waaiers-vite/src/RefreshWeatherData.jsx
+++ waaiers-vite/src/RefreshWeatherData.jsx
@@ -8,10 +8,27 @@ async function RefreshWeatherData (route_data)
     const route_time = route_data.route_time.substr(0,route_data.route_time.length-3);
 
     //First fetch the weather data points relavent to the route
-    const weatherPoints = await supabase.from('Weather').select('weather_lat,weather_lon').eq('route_id',route_id);
+    const weatherPoints = await supabase.from('Weather').select('weather_lat,weather_lon,id').eq('route_id',route_id);
     
     //Then requery the weather api
     const updatedWeatherData = await fetchWeatherData(null,weatherPoints.data,route_time,route_date)
-    console.log(updatedWeatherData)
+    
+    //Finally reformat the data to be updated on the databse
+    const windDirection = updatedWeatherData['wind.direction.at-10m'].data;
+    const windSpeed = updatedWeatherData['wind.speed.at-10m'].data;
+    const windSpeedGust = updatedWeatherData['wind.speed.gust.at-10m'].data;
+
+    const formattedUpdatedWeatherData = weatherPoints.data.map((item,index)=>({...item, weather_windspeed: windSpeed[index],weather_windgust: windSpeedGust[index], weather_winddir: windDirection[index]}))
+
+    //Update the database
+    
+    formattedUpdatedWeatherData.forEach((item)=> (updateWeatherDataQuery(item)));
+
+}
+
+async function updateWeatherDataQuery(item)
+{
+    const queryReturn = await supabase.from('Weather').update({weather_windspeed:item.weather_windspeed,weather_winddir: item.weather_winddir, weather_windgust: item.weather_windgust}).eq('id',item.id);
+    console.log(queryReturn)
 }
 export default RefreshWeatherData
