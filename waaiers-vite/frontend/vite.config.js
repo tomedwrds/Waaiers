c4e1323aa89a41b99875e89714ee61bddd4e7039
tomedwrds <tomedwards1056@outlook.com>
2025-02-19 14:53:31 +1300

    added dockerfile for front end

--- waaiers-vite/frontend/vite.config.js
+++ waaiers-vite/frontend/vite.config.js
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: "/",
   plugins: [react()],
-})
+  preview: {
+   port: 3000,
+   strictPort: true,
+  },
+  server: {
+   port: 3000,
+   strictPort: true,
+   host: true,
+   origin: "http://0.0.0.0:3000",
+  },
+ });
